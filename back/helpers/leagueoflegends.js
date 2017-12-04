a355e02546f6799cf8700d07c195228d0a3d1796
filipefborba <filipefborba@gmail.com>
2017-12-04 03:20:00 -0200

    external api requests ready

--- back/helpers/leagueoflegends.js
+++ back/helpers/leagueoflegends.js
@@ -1,127 +1,69 @@
-import {EventEmitter} from 'events'
-import auth from './auth.js';
+var serviceProfile = {
+    nickName: null,
+    serviceId: null,
+    steamProfile: {},
+    lolProfile: {},
+    battlenetProfile: {}
+};
 
-const emitter = new EventEmitter();
-emitter.setMaxListeners(20)
-
-var lolprofile = {
-                    summonerName: '',
-                    accountId: '',
-                    summonerId: '',
-                    summonerLevel: '',
-                    tier: '',
-                    rank: '',
-                    wins: '',
-                    losses: '',
-                    lastPlayedChampionId: '',
-                    lastPlayedChampion: '',
-                    mastery: {
-                        firstMasteryChampion: {
-                            level: '',
-                            points: '',
-                            championId: '',
-                            championName: ''},
-                        secondMasteryChampion: {
-                            level: '',
-                            points: '',
-                            championId: '',
-                            championName: ''}
-                        }
-                }
-
-var counter = 0
-
-export default window.leagueoflegends = {
-    subscribe(callback){
-        emitter.addListener('lolprofile_update', callback)
-        let user = auth.getUser()
-        user = JSON.parse(user)
-        lolprofile.summonerName = user.riot
-        this.getSummoner()
-    },
-
-    setData(){
-        emitter.emit('lolprofile_update',lolprofile)
-    },
-
-    getSummoner(summonerName) {
-        fetch('https://br1.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+ lolprofile.summonerName +'?api_key=RGAPI-467c2a2d-460d-4c5c-aac9-ddd55734fea0')
-            .then(lolrequest => lolrequest.json())
-            .then(lolrequest => {
-                lolprofile.summonerName  = lolrequest.name
-                lolprofile.summonerId    = lolrequest.id
-                lolprofile.accountId     = lolrequest.accountId
-                lolprofile.summonerLevel = lolrequest.summonerLevel
-                this.getRank(lolprofile.summonerId)
-                this.getChampionMasteries(lolprofile.summonerId)
-                this.setData()
-            })
-        },
-
-    getRank(summonerId) {
-        fetch('https://br1.api.riotgames.com/lol/league/v3/positions/by-summoner/'+ summonerId +'?api_key=RGAPI-467c2a2d-460d-4c5c-aac9-ddd55734fea0')
-            .then(lolrequest => lolrequest.json())
-            .then(lolrequest => {
-                lolprofile.tier   = lolrequest[0].tier
-                lolprofile.rank   = lolrequest[0].rank
-                lolprofile.wins   = lolrequest[0].wins
-                lolprofile.losses = lolrequest[0].losses
-                this.getLastPlayedChampionId(lolprofile.accountId)
-                this.setData()
-            })
-    },
-
-    getLastPlayedChampionId(accountId) {
-        fetch('https://br1.api.riotgames.com/lol/match/v3/matchlists/by-account/'+ accountId + '/recent?api_key=RGAPI-467c2a2d-460d-4c5c-aac9-ddd55734fea0')
-            .then(lolrequest => lolrequest.json())
-            .then(lolrequest => {
-                lolprofile.lastPlayedChampionId = lolrequest.matches[0].champion
-                this.getLastPlayedChampion(lolprofile.lastPlayedChampionId)
-                this.setData()
-            })
-    },
-
-    getLastPlayedChampion(championId) {
-        fetch('https://br1.api.riotgames.com/lol/static-data/v3/champions/'+ championId +'?locale=pt_BR&api_key=RGAPI-467c2a2d-460d-4c5c-aac9-ddd55734fea0')
-            .then(lolrequest => lolrequest.json())
-            .then(lolrequest => {
-                lolprofile.lastPlayedChampion = lolrequest.key
-                this.setData()
-            })
-    },
-
-    getMasteryChampions(firstChampionId, secondChampionId) {
-        fetch('https://br1.api.riotgames.com/lol/static-data/v3/champions/'+ firstChampionId +'?locale=pt_BR&api_key=RGAPI-467c2a2d-460d-4c5c-aac9-ddd55734fea0')
-            .then(lolrequest => lolrequest.json())
-            .then(lolrequest => {
-                lolprofile.mastery.firstMasteryChampion.championName = lolrequest.key
-                this.setData()
-            })
-        fetch('https://br1.api.riotgames.com/lol/static-data/v3/champions/'+ secondChampionId +'?locale=pt_BR&api_key=RGAPI-467c2a2d-460d-4c5c-aac9-ddd55734fea0')
-            .then(lolrequest => lolrequest.json())
-            .then(lolrequest => {
-                lolprofile.mastery.secondMasteryChampion.championName = lolrequest.key
-                this.setData()
-            })
-    },
-
-    getChampionMasteries(summonerId) {
-        fetch('https://br1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/'+ summonerId +'?api_key=RGAPI-467c2a2d-460d-4c5c-aac9-ddd55734fea0')
-            .then(lolrequest => lolrequest.json())
-            .then(lolrequest => {
-                lolprofile.mastery.firstMasteryChampion.level           = lolrequest[0].championLevel
-                lolprofile.mastery.firstMasteryChampion.points          = lolrequest[0].championPoints
-                lolprofile.mastery.firstMasteryChampion.championId      = lolrequest[0].championId
-                lolprofile.mastery.secondMasteryChampion.level          = lolrequest[1].championLevel
-                lolprofile.mastery.secondMasteryChampion.points         = lolrequest[1].championPoints
-                lolprofile.mastery.secondMasteryChampion.championId     = lolrequest[1].championId
-                this.getMasteryChampions(lolprofile.mastery.firstMasteryChampion.championId, lolprofile.mastery.secondMasteryChampion.championId)
-                this.setData()
-            })
-    },
+const key = 'RGAPI-ec4221fa-97ac-4723-bc89-1b672772dbda';
+var summonerName = 'coltshot';
 
 
-    getLolProfile(){
-        return lolprofile
-    }
+export default window.lol = {
+//para que todos os outros rodem, precisa do summonerID, entao vamos rodar antes
+getSummonerId() {
+    fetch('https://br1.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+summonerName+'?api_key='+key)
+    .then(res => res.json())
+    .then(res => {
+        serviceProfile.nickName  = res.name
+        serviceProfile.serviceId = res.id
+        serviceProfile.serviceSecondaryId = res.accountId
+    })
+},
+
+getLolInfo() {
+    var getSummonerByName = fetch('https://br1.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+summonerName+'?api_key='+key)
+        .then(function(response) {
+            return response.json()
+        });
+
+    var getLeague = fetch('https://br1.api.riotgames.com/lol/league/v3/positions/by-summoner/'+serviceProfile.serviceId+'?api_key='+key)
+        .then(function(response) {
+            return response.json()
+        });
+
+    var getLast20Matches = fetch('https://br1.api.riotgames.com/lol/match/v3/matchlists/by-account/'+serviceProfile.serviceSecondaryId+'/recent?api_key='+key)
+        .then(function(response) {
+            return response.json()
+        });
+
+    var getChampionMasteries = fetch('https://br1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/'+serviceProfile.serviceId+'?api_key='+key)
+        .then(function(response) {
+            return response.json()
+        });
+
+    var combinedData = {"getSummonerByName":{}, "getLeague":{}, "getLast20Matches":{}, "getChampionMasteries":{}};
+    Promise.all([getSummonerByName, getLeague, getLast20Matches, getChampionMasteries])
+        .then(function(values) {
+            combinedData["getSummonerByName"] = values[0];
+            combinedData["getLeague"] = values[1];
+            combinedData["getLast20Matches"] = values[2];
+            combinedData["getChampionMasteries"] = values[3];
+            serviceProfile.lolProfile = combinedData.getSummonerByName
+            serviceProfile.lolProfile.ranked = combinedData.getLeague[0]
+            serviceProfile.lolProfile.lastmatch = combinedData.getLast20Matches.matches[0]
+            serviceProfile.lolProfile.masteries = {}
+            serviceProfile.lolProfile.masteries.first = combinedData.getChampionMasteries[0]
+            serviceProfile.lolProfile.masteries.second = combinedData.getChampionMasteries[1]
+            serviceProfile.lolProfile.masteries.third = combinedData.getChampionMasteries[2]
+        });
+    
+    return combinedData;
+    
+},
+
+getServiceProfile() {
+    return serviceProfile
+}
 }
--- back/helpers/steam.js
+++ back/helpers/steam.js
@@ -1,32 +1,48 @@
-const serviceProfile = () => {
-    return {
-        nickName: null,
-        serviceId: null
-    }
-}
+var serviceProfile = {
+    nickName: null,
+    serviceId: null,
+};
 
-var steamConfig = '8DD3D47C1DFB6EA97EA7F6665C4FBA20';
+const key = '8DD3D47C1DFB6EA97EA7F6665C4FBA20';
+var steamids = '76561197996048272';
 
-let params = new URLSearchParams(Object.entries({
-    key : steamConfig,
-    steamids : '76561197996048272'
-}))
 
 export default window.steam = {
-    
-    getSteamInfo() {
-            fetch('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?' + params)
-                .then(res => res.json())
-                .then(data => {
-                    steamprofile.nickName      = steamrequest.response.players[0].personaname
-                    steamprofile.steamId       = steamrequest.response.players[0].steamid
-                    steamprofile.profileUrl    = steamrequest.response.players[0].profileurl
-                    steamprofile.avatarFullUrl = steamrequest.response.players[0].avatarfull
-                })
-                
-        },
+getSteamInfo() {
+    var getPlayerSummaries = fetch('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='+key+'&steamids='+steamids)
+        .then(function(response) {
+            return response.json()
+        });
 
-    // getSteamProfile(){
-    //     return steamprofile
-    // }
+    var getRecentlyPlayedGames = fetch('http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key='+key+'&steamid='+steamids)
+        .then(function(response) {
+            return response.json()
+        });
+
+    var getPlayerBans = fetch('http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key='+key+'&steamids='+steamids)
+        .then(function(response) {
+            return response.json()
+        });
+
+    var combinedData = {"getPlayerSummaries":{}, "getRecentlyPlayedGames":{}, "getPlayerBans":{}};
+    Promise.all([getPlayerSummaries, getRecentlyPlayedGames, getPlayerBans])
+        .then(function(values) {
+            combinedData["getPlayerSummaries"] = values[0];
+            combinedData["getRecentlyPlayedGames"] = values[1];
+            combinedData["getPlayerBans"] = values[2];
+            serviceProfile.nickName      = combinedData.getPlayerSummaries.response.players[0].personaname
+            serviceProfile.serviceId     = combinedData.getPlayerSummaries.response.players[0].steamid
+            serviceProfile.profileUrl    = combinedData.getPlayerSummaries.response.players[0].profileurl
+            serviceProfile.avatarFullUrl = combinedData.getPlayerSummaries.response.players[0].avatarfull
+            serviceProfile.last2weeksgames = combinedData.getRecentlyPlayedGames.response.games
+            serviceProfile.steamBans = combinedData.getPlayerBans.players[0]
+        });
+    
+    return combinedData;
+    
+},
+
+getServiceProfile() {
+    return serviceProfile
+}
 }
